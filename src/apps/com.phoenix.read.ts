5b2b108f12d0cf965873a81bed84bb4430651227
nanzhinet <155802941+nanzhinet@users.noreply.github.com>
2026-04-28 13:56:47 +0800

    更新红果新版和旧版

--- src/apps/com.phoenix.read.ts
+++ src/apps/com.phoenix.read.ts
@@ -1,5 +1,4 @@
 import { defineGkdApp } from '@gkd-kit/define';
-
 export default defineGkdApp({
   id: 'com.phoenix.read',
   name: '红果免费短剧',
@@ -15,6 +14,32 @@ export default defineGkdApp({
           name: '检测到「上滑继续观看短剧」时自动上滑',
           fastQuery: true,
           matches: '[vid="ang"][text="上滑继续观看短剧"][visibleToUser=true]',
+          swipeArg: {
+            start: {
+              x: 'screenWidth/2',
+              y: 'screenHeight*0.8',
+            },
+            end: {
+              x: 'screenWidth/2',
+              y: 'screenHeight*0.2',
+            },
+            duration: 300,
+          },
+          snapshotUrls: 'https://i.gkd.li/i/27098752',
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '全屏广告-红果短剧广告自动上滑(旧版)',
+      activityIds: [
+        'com.dragon.read.component.shortvideo.impl.ShortSeriesActivity',
+      ],
+      rules: [
+        {
+          name: '检测到「上滑继续观看短剧」时自动上滑',
+          fastQuery: true,
+          matches: '[vid="ana"][text="上滑继续观看短剧"][visibleToUser=true]',
           swipeArg: {
             start: {
               x: 'screenWidth/2',
